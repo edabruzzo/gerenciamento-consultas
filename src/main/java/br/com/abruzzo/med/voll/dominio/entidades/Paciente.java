a3445c391fb852668dedc457437841314eca5691
Emmanuel Abruzzo <e164330@trt2.jus.br>
2024-12-18 11:24:46 -0300

    Ajustes no application.yml e imports da entidade Paciente

--- src/main/java/br/com/abruzzo/med/voll/dominio/entidades/Paciente.java
+++ src/main/java/br/com/abruzzo/med/voll/dominio/entidades/Paciente.java
@@ -1,11 +1,15 @@
 package br.com.abruzzo.med.voll.dominio.entidades;
 
 import br.com.abruzzo.med.voll.dominio.enums.EspecialidadeEnum;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -18,6 +22,7 @@ public class Paciente extends Pessoa {
     private String crm;
 
     @Enumerated(EnumType.STRING)
-    private EspecialidadeEnum especialidadeEnum;
+    @Column(name = "in_especialidade")
+    private EspecialidadeEnum especialidade;
 
 }
